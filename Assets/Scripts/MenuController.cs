bf151c24aef331e5f6d09d4a9341475b9887cc6c
Elias <elias_piovani@hotmail.com>
2019-10-13 16:03:13 +0200

    Built for Windows, Mac and WebGL

--- Assets/Scripts/MenuController.cs
+++ Assets/Scripts/MenuController.cs
@@ -6,6 +6,12 @@ public class MenuController : MonoBehaviour
     public GameObject planet, spaceships;
     public float planetRotSpd = 2.0f, ssRotSpd = 2.0f;
 
+    private void Awake()
+    {
+        //Screen.fullScreenMode = FullScreenMode.Windowed;
+        //Screen.SetResolution(2048, 1536, true);
+    }
+
     // Start is called before the first frame update
     void Start()
     {
@@ -19,9 +25,9 @@ public class MenuController : MonoBehaviour
             SceneManager.LoadScene(1);
         }
 
-        planet.transform.Rotate(Vector3.up, planetRotSpd);
-        spaceships.transform.Rotate(Vector3.up, ssRotSpd);
-        spaceships.transform.Rotate(Vector3.right, ssRotSpd*0.2f);
+        planet.transform.Rotate(Vector3.up, planetRotSpd * Time.deltaTime);
+        spaceships.transform.Rotate(Vector3.up, ssRotSpd * Time.deltaTime);
+        spaceships.transform.Rotate(Vector3.right, ssRotSpd*0.2f * Time.deltaTime);
     }
 
     public void OnClickQuit()
